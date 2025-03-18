0b2111cbe87d5df936e1727aacd13670cf6e127a
aql <77629149+yuanweijiaopi@users.noreply.github.com>
2025-03-18 15:46:18 +0800

    1

---
+++ src/main/java/com/example/EmosExApiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosExApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosExApiApplication.class, args);
+    }
+
+}
