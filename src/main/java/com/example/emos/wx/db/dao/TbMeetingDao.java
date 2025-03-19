102362ee098e96c4c6e3e0b59f853b634928943d
aql <77629149+yuanweijiaopi@users.noreply.github.com>
2025-03-19 14:53:01 +0800

    1

---
+++ src/main/java/com/example/emos/wx/db/dao/TbMeetingDao.java
@@ -0,0 +1,17 @@
+package com.example.emos.wx.db.dao;
+
+import com.example.emos.wx.db.pojo.TbMeeting;
+
+public interface TbMeetingDao {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbMeeting record);
+
+    int insertSelective(TbMeeting record);
+
+    TbMeeting selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(TbMeeting record);
+
+    int updateByPrimaryKey(TbMeeting record);
+}
